1983efbb927b70869e21ac932a68345b3db69de6
Tuân Nguyễn <tuannt1930@gmail.com>
2021-07-10 23:15:53 +0700

    search component

---
+++ src/Component/Search/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Search(props) {
+
+    //API : https://api.themoviedb.org/3/search/person?api_key=cd58c7bd131cba3c391d62c5fda2ae53&language=en-US&query=${từ khóa search}&page=1&include_adult=false
+    // tìm hết các thứ search ra đưa ra danh sách gióng trang post
+    // tham khảo Component API
+    // thẻ input onChange 
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Search;
